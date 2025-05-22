75058a7a3c55ddb914d43ccd66ba37a443058129
JeeJ <gustavogomescoding@gmail.com>
2025-05-22 11:16:52 -0300

    .

---
+++ calendario.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <locale.h>
+
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	
+	int diaNasc, mesNasc, anoNasc;
+	
+	printf("Geração de calendário - Por Mr. JeeJ\n\n");
+	
+	printf("Digite sua data de nascimento (DD MM AAAA): ");
+	scanf("%d %d %d", &diaNasc, &mesNasc, &anoNasc);
+	
+	printf("%2d/%2d/%d", diaNasc, mesNasc, anoNasc);
+	
+	
+	return 0;
+}
+

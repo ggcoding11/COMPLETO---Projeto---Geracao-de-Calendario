abed74a6250e633e7c8124a089cc732cb3826368
JeeJ <gustavogomescoding@gmail.com>
2025-05-28 11:21:03 -0300

    Update calendario.c

--- calendario.c
+++ calendario.c
@@ -7,12 +7,11 @@ int main(){
 	
 	int diaNasc, mesNasc, anoNasc;
 	
-	printf("Geração de calendário - Por Mr. JeeJ\n\n");
+	printf("Geracao de calendario - Por Mr. JeeJ\n\n");
 	
 	printf("Digite sua data de nascimento (DD MM AAAA): ");
 	scanf("%d %d %d", &diaNasc, &mesNasc, &anoNasc);
 	
-	printf("%d/%d/%d", diaNasc, mesNasc, anoNasc);
 	
 	
 	return 0;

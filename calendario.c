72dc8f743b37d3b4bbebb36da39b15845044a838
Gustavo <gustavogomes1108@gmail.com>
2025-06-08 00:53:02 -0300

    .

--- calendario.c
+++ calendario.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <locale.h>
+#include <time.h>
+
 
 int main(){
 	setlocale(LC_ALL, "Portuguese");
@@ -10,8 +12,22 @@ int main(){
 	
 	printf("Digite sua data de nascimento (DD MM AAAA): ");
 	scanf("%d %d %d", &diaNasc, &mesNasc, &anoNasc);
+
+	time_t seg = time(NULL);
 	
+	struct tm tempoAtual = *(localtime(&seg));
 	
+	int diaAtual = tempoAtual.tm_mday;
+	int mesAtual = tempoAtual.tm_mon + 1;
+	int anoAtual = tempoAtual.tm_year + 1900;
+	
+	int idade = anoAtual - anoNasc;
+	
+	if (mesAtual < mesNasc || ((mesAtual == mesNasc) && (diaAtual < diaNasc))){
+		idade -= 1;
+	} 	
+	
+	printf("Idade: %d", idade);
 	
 	return 0;
 }
